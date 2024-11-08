704bf1097ef3f2df4a8ecb1002891a6a9fc21f7d
19harshitha <harshithasm034@gmail.com>
2024-11-08 19:12:40 +0530

    add

--- server/index.js
+++ server/index.js
@@ -13,9 +13,7 @@ const userRoutes = require("./routes/user");
 const app = express();
 
 app.use(cors());
-app.use(cors({
-  origin: 'https://story-slides-7ccf.vercel.app'
-}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
